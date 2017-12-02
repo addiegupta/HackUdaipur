d48f15bde052b0d9a6b17cb781dd8c1a22ff175c
Aditya <addiegupta@gmail.com>
2017-12-02 18:15:06 +0530

    Create diagnosis and post diagnosis activity and other fixes

---
+++ app/src/main/java/com/example/android/hackudaipur/ui/PostDiagnosisActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.hackudaipur.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.hackudaipur.R;
+
+public class PostDiagnosisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_diagnosis);
+    }
+}
--- app/src/main/java/com/example/android/hackudaipur/ui/SymptomsActivity.java
+++ app/src/main/java/com/example/android/hackudaipur/ui/SymptomsActivity.java
@@ -1,23 +1,24 @@
 package com.example.android.hackudaipur.ui;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.android.hackudaipur.R;
 import com.example.android.hackudaipur.adapter.SymptomAdapter;
-import com.example.android.hackudaipur.data.ListColumns;
+import com.example.android.hackudaipur.data.SymptomColumns;
 import com.example.android.hackudaipur.model.Symptom;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -51,80 +52,76 @@ public class SymptomsActivity extends AppCompatActivity {
 
         mSymptomsList = new ArrayList<>();
 
-        /*Cursor cursor = getContentResolver().query(URI_USERS, null, null, null, null);
+        Cursor cursor = getContentResolver().query(URI_SYMPTOMS, null, null, null, null);
         if (cursor != null) {
             cursor.moveToFirst();
             while (!cursor.isAfterLast()) {
-                String userName = cursor.getString(cursor.getColumnIndexOrThrow(ListColumns.NAME));
-                int id = cursor.getInt(cursor.getColumnIndexOrThrow(ListColumns._ID));
-
-                mSymptomsList.add(new Symptom(userName, false));
-                cursor.moveToNext();
-            }
-            cursor.close();
-        }*/
-        Cursor cursor = getContentResolver().query(URI_USERS, null, null, null, null);
-        if (cursor != null) {
-            cursor.moveToFirst();
-            while (!cursor.isAfterLast()) {
-                String userName = cursor.getString(cursor.getColumnIndexOrThrow(ListColumns.NAME));
+                String userName = cursor.getString(cursor.getColumnIndexOrThrow(SymptomColumns.NAME));
                 mSymptomsList.add(new Symptom(userName, false));
                 cursor.moveToNext();
             }
             cursor.close();
         }
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
         mRVSymptoms.setHasFixedSize(true);
 
-        // create an Object for Adapter
         mAdapter = new SymptomAdapter(mSymptomsList);
 
-        // set the adapter object to the Recyclerview
         mRVSymptoms.setAdapter(mAdapter);
         mShowButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                String secemp = "";
+
+                //TODO Change symptoms selection mechanism and place it in a method
+                /*
+                String selectedSymptoms = "";
                 List<Symptom> stList = ((SymptomAdapter) mAdapter).getSymptomList();
 
                 for (int i = 0; i < stList.size(); i++) {
                     Symptom symptom = stList.get(i);
                     if (symptom.isSelected()) {
 
-                        secemp = secemp + "\n" + symptom.getSymptomName();
+                        selectedSymptoms += "\n" + symptom.getSymptomName();
                     }
 
-                }
+                }*/
+                showDiagnosisSelectionDialogBox();
 
-                Toast.makeText(SymptomsActivity.this,
-                        "Selected Employees: \n" + secemp, Toast.LENGTH_LONG)
-                        .show();
             }
         });
     }
 
-//    @Override
-//    public void onClick(Symptom selectedSymptom) {
-//
-////        selectedSymptom.setSelected();
-////
-////        viewHolder.chkSelected.setTag(symptomList.get(position));
-////        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-////            public void onClick(View v) {
-////                CheckBox cb = (CheckBox) v;
-////                Symptom symptom = (Symptom) cb.getTag();
-////
-////                symptom.setSelected(cb.isChecked());
-////                symptomList.get(pos).setSelected(cb.isChecked());
-////
-////                Toast.makeText(
-////                        v.getContext(),
-////                        "Selected Symptoms: " + cb.getText() + " is "
-////                                + cb.isChecked(), Toast.LENGTH_LONG).show();
-////            }
-////        });
-//    }
+    private void showDiagnosisSelectionDialogBox() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.continue_to_diagnosis)
+                .setMessage(R.string.continue_to_diagnosis_message)
+                .setPositiveButton(R.string.conti, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        continueWithDiagnosis(true);
+                    }
+                })
+                .setNegativeButton(R.string.skip_diagnosis, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        continueWithDiagnosis(false);
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    private void continueWithDiagnosis(boolean diagnose) {
+        finish();
+        //TODO Put symptoms data
+
+        if (diagnose) {
+            startActivity(new Intent(SymptomsActivity.this, DiagnosisActivity.class));
+        } else {
+            startActivity(new Intent(SymptomsActivity.this, PostDiagnosisActivity.class));
+        }
+
+    }
 }
